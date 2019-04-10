bbb115f94d6e303e4961ac265a9e2754c90d6de8
Ycandy <candy_ye123@outlook.com>
2019-04-10 15:50:19 +0800

    文件下载和通知公告页面

--- src/views/home/passage.vue
+++ src/views/home/passage.vue
@@ -1,9 +1,30 @@
 <template lang="pug">
   .container
+    .banner
+      img()
+      span 通知公告
+    .list-board
+      .list-item(v-for='item in data')
+        .title
+          .article {{ item.title }}
+          .download
+          .time {{ item.time }}
+        .detail {{ item.detail }}
+    .page
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      data: [{ title: '北师大', time: '2019-03-09', detail: '123ashdpfhapuehpauh' },
+        { title: '北师大', time: '2019-03-09', detail: '123ashdpfhapuehpauh' },
+        { title: '北师大', time: '2019-03-09', detail: '123ashdpfhapuehpauh' },
+        { title: '北师大', time: '2019-03-09', detail: '123ashdpfhapuehpauh' },
+        { title: '北师大', time: '2019-03-09', detail: '123ashdpfhapuehpauh' },
+        { title: '北师大', time: '2019-03-09', detail: '123ashdpfhapuehpauh' }]
+    }
+  }
 }
 </script>
 
@@ -12,4 +33,57 @@ export default {
   display flex
   flex-flow: column nowrap
   flex 1
+  align-items center
+.list-board
+  padding-top 30px
+  max-width 1060px
+  width 100%
+  flex 1
+  display flex
+  flex-flow: column nowrap
+.page, .banner
+  width 100%
+  flex none
+  display flex
+  flex-flow: column nowrap
+  justify-content center
+  align-items center
+.banner
+  height 40px
+  background bottom center
+  background-size cover
+.page
+  height 50px
+  margin 50px 0 60px 0
+.list-item
+  width 100%
+  display flex
+  flex-flow column nowrap
+  padding-bottom 30px
+  margin-top 30px
+  border-bottom 1px solid #e9e9e9
+  .title
+    display flex
+    flex-flow row nowrap
+    cursor pointer
+    .article 
+      color #333333
+      max-width 960px
+      line-height 22px
+      overflow hidden
+      white-space nowrap
+      text-overflow ellipsis
+      flex 1
+    .time
+      color #b0b0b0
+      width 100px
+      flex none
+  .detail
+    margin-top 16px
+    line-height 18px
+    max-width 960px
+    color #999999
+    overflow hidden
+    white-space nowrap
+    text-overflow ellipsis
 </style>
