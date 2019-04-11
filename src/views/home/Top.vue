c63ca4b0095b91672e3ca1b0845cc8019578ecb7
Qian Zhang <qian.zhang@geneegroup.com>
2019-04-11 11:08:26 +0800

    top styl

--- src/views/home/Top.vue
+++ src/views/home/Top.vue
@@ -28,26 +28,5 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.top
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 1200px;
-  min-width: 1100px;
-  height: 100px;
-  align-items: center;
-.entrance
-  font-size: 14px;
-  width: 100px;
-  height: 32px;
-  line-height: 32px;
-  border: 1px solid #e27471;
-  border-radius: 4px;
-  text-align: center;
-  box-sizing: border-box;
-  cursor: pointer;
-.image
-  width: 500px;
-  height: 58px;
 </style>
 
